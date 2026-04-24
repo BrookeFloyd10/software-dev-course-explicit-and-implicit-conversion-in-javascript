05d0738bfcad49d8be00bf965a998ad6ee94353b
Brooke F <brooke.floyd10@gmail.com>
2026-04-23 23:31:31 -0500

    added spaces for output readability, added  console.logs to display values before and after conversion

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -41,8 +41,6 @@ if (isValid) {
 let age = Number("25"); // This converts "25" from a string to 25 a number so it will be added instead of concatenated
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
-console.log("\nBefore conversion age is a string: ''25'' " );
-console.log("After conversion age is a number: " + age);
 
 // Implicit Type Conversion
 let olderSister = 38; // value is number
@@ -50,12 +48,14 @@ let youngerSister = "15"; // value is a string
 let olderSisterDifference = olderSister - age; // subtracts a number from a number
 let youngerSisterDifference = age - youngerSister; // demonstrates how JS will implicitly turn a string into a number when using the (-) operator
 console.log("\nI have 2 sisters. My big sister is " + olderSisterDifference + " years older, and my little sister is " + youngerSisterDifference + " years younger than me.");
+console.log("\nBefore conversion age is a string: ''25'' " );
+console.log("After conversion age is a number: " + age);
 
 
 // Explicit Type Conversion
 let dogs = 2;
 let cats = 3;
 let reptiles = Number(null) // Number() converts null into a number so it shows up as 0 instead of a string in the final result
-console.log("I love my pets, I have " + dogs + " dogs " + cats + " cats and " + reptiles + " reptiles.");
+console.log("\nI love my pets, I have " + dogs + " dogs " + cats + " cats and " + reptiles + " reptiles.");
 console.log("\nBefore conversion reptiles = : " + null);
 console.log("After conversion reptiles = : " + reptiles);
